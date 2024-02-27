d9adfdfd83366e93591db803189943ed060c5170
theophilusnuer <tphilus003@gmail.com>
2024-02-27 16:01:02 +0000

    worked on the search box

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -1,36 +1,42 @@
 import { Card, CardContent, CardMedia, Container, Grid, TextField, Typography, CardActionArea } from "@mui/material";
 import { useEffect, useState } from "react";
+import emptyIcon from '../../assets/images/undraw_no_data_re_kwbl.svg';
 
 export default function Recipes() {
     const [recipes, setRecipes] = useState([]);
+    const [keyword, setKeyword] = useState('');
 
     const getRecipes = () => {
             //prepare URL
             const url = new URL ('https://api.spoonacular.com/recipes/complexSearch');
-            url.searchParams.append('apiKey', 'beb93f5709004fd39b99700f019f6466');
+            url.searchParams.append('apiKey', process.env.REACT_APP_SPOONACULAR_API_KEY);
+            url.searchParams.append('query', keyword);
         // fetch recipes from API
         fetch(url)
             .then(response => response.json())
             .then(data => {
                 //update the recipes state
                 setRecipes(data.results);
+                
             })
             .catch(error => {
                 console.log(error);
             })
 
     }
-useEffect(getRecipes, [])
+useEffect(getRecipes, [keyword])
     return (
         <Container sx={{ my: '1rem' }}>
 
             <TextField fullWidth
                 id="outlined-basic"
                 label="Enter a keyboard to search recipes and hit enter"
-                variant="outlined" />
+                variant="outlined" 
+                onKeyDown={event => event.key === 'Enter' && setKeyword(event.target.value)}
+                />
 
             <Grid sx={{ mt: '0.5rem' }} container spacing={3}>
-              { recipes.map(recipe => ( <Grid key={recipe.id} item xs={4}>
+              { recipes.length > 0 ? recipes.map(recipe => ( <Grid key={recipe.id} item xs={4}>
                     <Card sx={{ maxWidth: 345, height: '100%' }}>
                         <CardActionArea sx={{height: '100%'}}>
                             <CardMedia
@@ -47,7 +53,7 @@ useEffect(getRecipes, [])
                             </CardContent>
                         </CardActionArea>
                     </Card>
-                </Grid>))}
+                </Grid>)) : <img src={emptyIcon} width='50%' alt=''/>}
             </Grid>
         </Container>
     );
